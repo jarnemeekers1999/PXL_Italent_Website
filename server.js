5750df126ab1e34fd22b904bb2075364db74e667
Jarne Meekers <32525219+jarnemeekers1999@users.noreply.github.com>
2021-04-28 23:20:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/italentWebsite'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/italentWebsite/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
